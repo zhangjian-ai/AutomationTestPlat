ebd7a4615ea37668055da17e4c60c9d0f1d5e7d4
zhangjian <jian.zhang7@jiediankeji.com>
2021-09-09 19:39:42 +0800

    部署准备

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -158,6 +158,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# 设置静态文件收集目录
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'frontend/static')
+# Django访问静态文件的路径，相对于BASE_DIR
 STATIC_URL = '/static/'
 
 # 配置日志器，logger = logging.getLogger('test_plat')

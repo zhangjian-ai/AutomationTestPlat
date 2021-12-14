99756c9957c1639f9213ba97ee20f16891cfdade
zhangjian <jian.zhang7@jiediankeji.com>
2021-12-14 23:34:49 +0800

    修改 docker 网络配置，原网段已经被已有服务占用

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 
@@ -15,11 +16,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 from .routing import urlpatterns
 
+django.setup()
+
 # 配置文件上线时需要修改为线上配置文件
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
-
-import django
-django.setup()
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
